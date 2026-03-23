071f092ddd556dac90b04ab934a11e647ce793b5
celestinolopes <celestino.designerweb@gmail.com>
2026-03-23 13:33:19 +0100

    first commit

--- navigation/ContentView.swift
+++ navigation/ContentView.swift
@@ -7,15 +7,40 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    @State private var name: String = ""
+    @State private var age: String = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                 Spacer().frame(height: 20)
+                
+                TextField("Insert your name", text: $name)
+                    .keyboardType(.alphabet)
+                    .padding(10)
+                    .overlay(Rectangle().stroke())
+                    .cornerRadius(2)
+                
+                Spacer().frame(height: 25)
+                
+                TextField("Insert your age", text: $age)
+                    .keyboardType(.numberPad)
+                    .padding(10)
+                    .overlay(Rectangle().stroke())
+                    .cornerRadius(2)
+                
+                Spacer()
+                NavigationLink(destination:ProcessView(name: "\(name)", age: "\(age)"),label: {
+                    Text("Okay")
+                        .font(.title)
+                        .padding()
+                        .overlay(Capsule().stroke())
+                })
+            }.navigationTitle("Navigation Application")
+            .padding()
         }
-        .padding()
     }
 }
 
--- navigation/ProcessView.swift
+++ navigation/ProcessView.swift
@@ -8,11 +8,33 @@
 import SwiftUI
 
 struct ProcessView: View {
+    let name: String
+    var age: String
+    var isValidAge: Bool{
+        
+        if let convertedAge = Int(age){
+            return convertedAge>18
+        }
+        else{
+            return false
+        }
+    }
+    
+    var colorBackground : Color {
+        isValidAge ? Color.green : Color.red
+    }
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            colorBackground.ignoresSafeArea()
+            
+            Text("Welcome to board \(name), your age \(age) is valid").padding(10)
+        }
     }
 }
 
 #Preview {
-    ProcessView()
+    ProcessView(name: "Celestino", age: "12")
 }
+
