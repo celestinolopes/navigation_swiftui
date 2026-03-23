b6889b5e209ea5874625f5668fed9df042b96c63
celestinolopes <celestino.designerweb@gmail.com>
2026-03-23 10:39:08 +0100

    Initial Commit

---
+++ navigation/ProcessView.swift
@@ -0,0 +1,18 @@
+//
+//  ProcessView.swift
+//  navigation
+//
+//  Created by Celestino Lopes on 23/03/26.
+//
+
+import SwiftUI
+
+struct ProcessView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProcessView()
+}
---
+++ navigation/navigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigationApp.swift
+//  navigation
+//
+//  Created by Celestino Lopes on 23/03/26.
+//
+
+import SwiftUI
+
+@main
+struct navigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
